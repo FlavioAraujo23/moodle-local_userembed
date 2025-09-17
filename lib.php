18dac39d9267b23f5c90b4ec94e6d56fae3b006c
FlavioAraujo23 <flaviolopes1027@gmail.com>
2025-09-17 11:31:07 -0300

    add link in profile config

--- lib.php
+++ lib.php
@@ -1,25 +1,50 @@
 <?php
+
 defined('MOODLE_INTERNAL') || die();
 
-function local_userembed_user_profile_view($user, $course)
+/**
+ * Adiciona item no menu dropdown do usuário
+ */
+function local_userembed_extend_navigation_user($navigation, $user, $usercontext, $course, $coursecontext)
 {
-    if (!get_config('local_userembed', 'showinprofile')) {
-        return;
+    if (
+        has_capability('local/userembed:viewembed', context_system::instance()) &&
+        get_config('local_userembed', 'showinprofile') &&
+        isloggedin() && !isguestuser()
+    ) {
+
+        $url = new moodle_url('/local/userembed/view.php', ['id' => $user->id]);
+        $navigation->add(
+            get_string('linknameinmenuprofile', 'local_userembed'),
+            $url,
+            navigation_node::TYPE_SETTING,
+            null,
+            'userembed_view',
+            new pix_icon('i/user', '', 'core')
+        );
     }
+}
 
-    if (has_capability('local/userembed:viewembed', context_system::instance())) {
-        if (get_config('local_userembed', 'showinprofile')) {
-            if (isloggedin() && !isguestuser()) {
-                global $USER;
+/**
+ * Adiciona nas configurações do usuário
+ */
+function local_userembed_extend_settings_navigation($settingsnav, $context)
+{
+    global $USER;
 
-                $url = new moodle_url('/local/userembed/view.php', ['id' => $USER->id]);
-                $navigation->add(
-                    get_string('linknameinmenuprofile', 'local_userembed'),
-                    $url,
-                    navigation_node::TYPE_SETTING
-                );
-            }
+    if (
+        has_capability('local/userembed:viewembed', context_system::instance()) &&
+        get_config('local_userembed', 'showinprofile') &&
+        isloggedin() && !isguestuser()
+    ) {
+
+        if ($usernode = $settingsnav->find('usercurrentsettings', navigation_node::TYPE_CONTAINER)) {
+            $url = new moodle_url('/local/userembed/view.php', ['id' => $USER->id]);
+            $usernode->add(
+                get_string('linknameinmenuprofile', 'local_userembed'),
+                $url,
+                navigation_node::TYPE_SETTING
+            );
         }
     }
-
 }
